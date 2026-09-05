cec35cd98f6edc1c91c5feea98294db4a7a5c1ef
Orestis Sinis <orestisinis@gmail.com>
2026-09-04 21:02:25 -0400

    new

--- assets/google-sheet-config-round1118.js
+++
@@ -1,3 +0,0 @@
-/* Automated Hearts — Google Sheets form endpoint.
-   After deploying google-apps-script/Code.gs as a Web App, paste the /exec URL below. */
-window.AH_GOOGLE_SHEETS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzRT1Z-3GUQaRctYcN7_c-v2QEdVtzV4Yi4dFmiAt0-Ip_04p276QEGDDTPCHi8BVeMJQ/exec';
